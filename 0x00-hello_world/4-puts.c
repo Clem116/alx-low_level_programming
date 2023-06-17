af318fabefec3d55061bf65197da97486370d1f3
Clem116 <clementineonami@gmail.com>
2023-06-17 02:36:24 -0700

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+/**
+ *  Program that prints a string followed by a new line using a put function 
+ *  Return: 0(Success)
+ */
+int main ()
+{
+	puts ("Programming is like building a multilingual puzzle")
+		return 0;
+}
