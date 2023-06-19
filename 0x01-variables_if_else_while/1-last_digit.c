eab8f5ae7d717d5f948c91dd82f56237035a711a
Clem116 <clementineonami@gmail.com>
2023-06-19 13:30:57 -0700

    lsat digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdio.h>
-/**
- * main - Entry point
- * Return: Always (0) Success
+#include <stdlib.h>
+/** 
+ *main - Your main program logic goes here
+ *Return: Always (0) Success
  */
 int main(void)
 {
-int n, lastd;
+int n;
+int ld;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-lastd = n % 10;
-
-if (lastd > 5)
+ld = n % 10;
+if (ld > 5)
 {
-printf("The last digit of &d is &d and is greater than 5\n", n, lastd);
+printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
-else if (lastd == 0)
+else if (ld == 0)
 {
-printf("The last digit of %d is %d and is 0\n", n, lastd);
+printf("Last digit of %d is %d and is 0", n, ld);
 }
-else if (lastd < 6 && lastd != 0);
+else
 {
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+printf("Last digit of %d is %d and is less than 6 and not 0", n, ld);
 }
 return (0);
 }
-
