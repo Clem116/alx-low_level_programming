7bc634d2ff6ffb2442ad13089a0d85a3ae90c49b
Clem116 <clementineonami@gmail.com>
2023-06-19 16:41:56 -0700

    prints

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+int c;
+c = 0;
+while
+(c < 10)
+{
+putchar('c');
+c++;
+}
+putchard('\n');
+return (0);
