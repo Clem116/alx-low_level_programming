b2036596d1ceda2b4fd1c3aff248d74ded0abc24
Clem116 <clementineonami@gmail.com>
2023-06-21 15:39:06 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'prints the alphabet in lowercase'
+ * Return: always (0) Success
+ */
+int 2-print_alphabet_x10.c(void)
+int i;
+int j
+for (j = 1; i <= 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
