1997bf7a39de81e3f679c965c6b282c2ca474ad1
Clem116 <clementineonami@gmail.com>
2023-06-23 13:23:46 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,14 @@ void print_line(int n)
 {
 if (n <= 0)
 {
-putchar('\n');
-}
-else
+_putchar('\n');
+} else
 {
 int i;
 for (i = 1; i <= n; i++)
 {
-putchar('_');
+_putchar('_');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
