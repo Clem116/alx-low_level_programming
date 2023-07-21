d3c5605f33a725161d45b4c3846a28378a7899ea
Clem116 <clementineonami@gmail.com>
2023-07-20 19:49:09 -0700

    any

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,25 +5,27 @@
  * print_all - prints everything
  * @format: list typesof arguments passed
  */
+void print_all(const char * const format, ...)
+{
 int i = 0;
 char *str, *sep = "";
-va_list list;
-va_list(list, format);
+va_list any;
+va_start (any, format);
 if (format)
 {
 while (format[i])
 {
 case 'c':
-printf("%s%c", sep, va_arg(list, int));
+printf("%s%c", sep, va_arg(any, int));
 break;
 case 'i':
-printf("%s%d", sep, va_arg(list, int));
+printf("%s%d", sep, va_arg(any, int));
 break;
 case 'f':
-printf("%s%f", sep, va_arg(list, double));
+printf("%s%f", sep, va_arg(any, double));
 break;
 case 's':
-str = va_arg(list, char*);
+str = va_arg(any, char*);
 if (!str)
 str = "(nil)";
 printf("%s%s", sep, str);
@@ -37,5 +39,5 @@ i++;
 }
 }
 printf("\n");
-va_end(list);
+va_end(any);
 }
