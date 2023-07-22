db767321e9c944bf19f91ec87cd2b7019c1fa6b6
Clem116 <clementineonami@gmail.com>
2023-07-21 20:06:52 -0700

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,7 +8,7 @@
  *
  * Return: 0
  */
-int int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int(*cmp)(int))
 {
 int i;
 if (array == NULL || size <= 0 || cmp == NULL)
@@ -18,5 +18,5 @@ for (i = 0; i < size; i++)
 if (cmp(array[i]))
 return (i);
 }
-return (0);
+return (-1);
 }
