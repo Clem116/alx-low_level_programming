6cf767b30c2bd9a0963c84d9d65d139be3dd2d2f
Clem116 <clementineonami@gmail.com>
2023-07-28 14:02:19 -0700

    add node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ printf("[0] (nil)\n");
 }
 else
 {
-printf("[%lu] %s\n", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 }
 
 h = h->next;
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - addition of a new node at the start of list
+ * @head: points to the head of list
+ * @str: added string
+ *
+ * Return: pointer to the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_node;
+unsigned int len = 0;
+
+if (str[len])
+len++;
+
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+
+{
+free(new_node);
+return (NULL);
+}
+new_node->str = strdup(str);
+new_node->len = len;
+new_node->next = (*head);
+(*head) = new_node;
+
+return (*head);
+}
