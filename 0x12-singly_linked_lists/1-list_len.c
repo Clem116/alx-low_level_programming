6fe6b7885d92ba9c86b10d5acb1723d1e9b9f5e7
Clem116 <clementineonami@gmail.com>
2023-07-28 10:37:35 -0700

    b

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
-/* Definition of a list structure */
-typedef struct no_of_elements
-	{
-	char *str;
-	size_t len;
-	struct no_of_elements *next;
-	}list_t;
+
 /**
  * list_len - print number of elements in a linked list
  * @h: points to head of a linked list
  *
  * Return: Number pf elements in a node
  */
-size_t no_of_elements(const list_t *h)
+size_t list_len(const list_t *h)
 {
 size_t node = 0;
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef _LIST_H
 #define _LIST_H
-#include <stddef.h>
 /**
  * struct list_s - Singly linked list
  * @str: string parameter
@@ -9,13 +8,13 @@
  *
  * Description: structure to a linked list
  */
-typedef struct no_of_elements
+typedef struct list_s
 {
 char *str;
-size_t len;
-struct no_of_elements *next;
+unsigned int len;
+struct list_s *next;
 } list_t;
 /* Function prototype for printing all eements of a list */
 size_t print_list(const list_t *h);
-size_t no_of_elements(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
