cbb3245a91e0fbbbc5ed60d68070b7a4d96d1829
Clem116 <clementineonami@gmail.com>
2023-07-28 08:38:54 -0700

    print a list of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all elemnts of a t_list
+ * @h: points to the list head
+ *
+ * Return: Nodes of the list
+ */
+size_t print_list(const list_t *h)
+{
+size_t node = 0;
+
+while (h != NULL)
+{
+printf("[%lu] %s\n", h->len, h->str);
+h = h->next;
+node++;
+}
+return (node);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef _LIST_H
+#define _LIST_H
+#include <stddef.h>
+/**
+ * struct list_s - Singly linked list
+ * @str: string parameter
+ * @len: string length
+ * @next: contains address to the next node
+ *
+ * Description: structure to a linked list
+ */
+typedef struct list_s
+	{	
+	char *str;
+	size_t len;
+	struct list_s *next;
+	} list_t;
+/* Function prototype for printing all eements of a list */
+size_t print_list(const list_t *h);
+#endif 
+/* LIST_H */
