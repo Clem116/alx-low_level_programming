e761f0dd502ff596d9d8c5b9624c38d8c3296a1d
Clem116 <clementineonami@gmail.com>
2023-08-01 15:17:16 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - prints linked list elements
+ * @h: traverse
+ *
+ * Return: nodes
+ */
+size_t num = 0;
+
+while (h)
+{
+	num++
+	h = h->next;
+}
+return (num);
+}
+
