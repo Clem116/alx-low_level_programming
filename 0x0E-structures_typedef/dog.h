43a3ec26306273a329bf1dc259001d82f4016593
Clem116 <clementineonami@gmail.com>
2023-08-01 14:24:29 -0700

    me

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,10 @@ char *owner;
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
+
+int _strlen(char *s);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
